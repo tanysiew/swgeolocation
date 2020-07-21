485e138d3dc1bfad5ca62710804ed1177bf74bf3
Siewdio <tanysiew@hotmail.com>
2020-07-21 22:09:17 +0800

    Testing for config

---
+++ config/db.js
@@ -0,0 +1,21 @@
+const { Mongoose } = require("mongoose")
+
+const mongoose = require('mongoose');
+
+const connectDB = async() => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
+            useUnifiedTopology: true
+        });
+
+        console.log(`MongoDb connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
